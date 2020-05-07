51cfaaf5d07b9fa012f8ce88969b018ceee119b3
xmagnusf <magnus.jensen.fuglerud@nhn.no>
2020-05-07 20:00:54 +0200

    endret til fd-url

--- src/test/scala/magnus/CMSimulation.scala
+++ src/test/scala/magnus/CMSimulation.scala
@@ -6,7 +6,8 @@ import io.gatling.http.Predef._
 class CMSimulation extends Simulation{
 
   val httpProtocol = http
-    .baseUrl("https://helsenorge-perftest.azurefd.net")
+    //.baseUrl("https://helsenorge-perftest.azurefd.net")
+    .baseUrl("http://helsenorge-perftest.azureedge.net")
     .disableCaching
 
   val headers_0 = Map(
