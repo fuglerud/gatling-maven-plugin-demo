0d9c7fadb3600051a786d575e47a905ca92eca25
xmagnusf <magnus.jensen.fuglerud@nhn.no>
2020-05-08 10:45:50 +0200

    endret til fd-url

--- src/test/scala/magnus/OpenModelCMS.scala
+++ src/test/scala/magnus/OpenModelCMS.scala
@@ -43,10 +43,10 @@ class OpenModelCMS extends Simulation{
         .check(status.is(expected = 200))
         .check(regex("<title>Allergisk sjokk</title>")))
 
-  //setUp(scn.inject(atOnceUsers(1))).protocols(httpProtocol)
+  setUp(scn.inject(atOnceUsers(1000))).protocols(httpProtocol)
   //setUp(scn.inject(rampUsersPerSec(1) to 5 during (30),constantUsersPerSec(5) during(60))).protocols(httpProtocol)
   //setUp(scn.inject(constantUsersPerSec(500) during(60))).protocols(httpProtocol)
   //setUp(scn.inject(rampConcurrentUsers(5) to(200) during(120))).protocols(httpProtocol)
   //setUp(scn.inject(constantConcurrentUsers(10) during (120), rampConcurrentUsers(10) to (100) during (120))).protocols(httpProtocol)
-  setUp(scn.inject(incrementUsersPerSec(5).times(5).eachLevelLasting(10).separatedByRampsLasting(10).startingFrom(10))).protocols(httpProtocol)
+  //setUp(scn.inject(incrementUsersPerSec(5).times(5).eachLevelLasting(10).separatedByRampsLasting(10).startingFrom(10))).protocols(httpProtocol)
 }
