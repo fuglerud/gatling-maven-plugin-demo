0c7612161dd34b38e2813ce9cbe44e40f1ffa453
fuglerud <majen78@gmail.com>
2020-06-07 08:31:52 +0200

    lagt til en 0

--- src/test/scala/magnus/ContentAPISimulation.scala
+++ src/test/scala/magnus/ContentAPISimulation.scala
@@ -36,13 +36,14 @@ class ContentAPISimulation extends Simulation{
       .queryParam("Maalgruppe","${maalgruppe}")
       .check(status.is(expected = 200)))
 
+  val selectedProfile = System.getProperty("selectedProfile") match {
+    case "profile1" => scn.inject(atOnceUsers(1))
+    case "profile2" => scn.inject(rampUsersPerSec(1) to 5 during (30),constantUsersPerSec(5) during(600))
+    case "profile3" => scn.inject(constantUsersPerSec(500) during(60))
+    case "profile4" => scn.inject(rampConcurrentUsers(5) to(200) during(120))
+    case "profile5" => scn.inject(constantConcurrentUsers(10) during (120), rampConcurrentUsers(10) to (100) during (120))
+    case "profile6" => scn.inject(incrementUsersPerSec(5).times(5).eachLevelLasting(10).separatedByRampsLasting(10).startingFrom(10))
 
-
-  //setUp(scn.inject(atOnceUsers(5))).protocols(httpProtocol)
-  setUp(scn.inject(rampUsersPerSec(1) to 5 during (30),constantUsersPerSec(5) during(60))).protocols(httpProtocol)
-  //setUp(scn.inject(constantUsersPerSec(500) during(60))).protocols(httpProtocol)
-  //setUp(scn.inject(rampConcurrentUsers(5) to(40) during(120))).protocols(httpProtocol)
-  //setUp(scn.inject(constantConcurrentUsers(10) during (120), rampConcurrentUsers(10) to (100) during (120))).protocols(httpProtocol) 
-  //setUp(scn.inject(incrementUsersPerSec(5).times(5).eachLevelLasting(10).separatedByRampsLasting(10).startingFrom(10))).protocols(httpProtocol)
+  }
 
 }
