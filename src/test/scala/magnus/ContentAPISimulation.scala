ea762b21b92656952e7dc7a9038d6381213c7e21
fuglerud <majen78@gmail.com>
2020-06-08 17:43:01 +0200

    lagt til en 0

--- src/test/scala/magnus/ContentAPISimulation.scala
+++ src/test/scala/magnus/ContentAPISimulation.scala
@@ -14,7 +14,7 @@ class ContentAPISimulation extends Simulation{
 
   val headers_1 = Map(
     "Content-Type" -> "application/xml",
-   // "Authorization" -> "Bearer ${access_token}",
+   // "Authorization" -> "Bearer ${access_token}"
     "Authorization" -> "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsInZlciI6MSwidHlwXzIiOiJyZWYifQ.eyJqdGkiOiJiZjBhZGFlNy1kYzkwLTQ0YmEtOTNiMC0wMDRiM2NlMzFkZDciLCJjbGllbnRfaWQiOiIxN2NlODA2My1hYmRjLTQyNjYtYTUyNC1kYzk3ZTBhMzA1ZWEiLCJjbGllbnRfbmFtZSI6IkZlbGxlcyBOZXR0bMO4c25pbmcgRm9yIFNwZXNpYWxpc3RoZWxzZXRqZW5lc3RlbiIsInJlZl90b2tlbiI6IjdWQWNyUnItdHJ3d0YzNVR2cEhkbGxFV2RyMHdiZGpXZXAySkh6cEtPcnMiLCJyZWZfdG9rZW5fdHlwZSI6IkZ1bGwiLCJ6b25lIjoiZWtzdGVybl9pbnRlcm5ldHQiLCJlbmR1c2VydHlwZSI6InN5c3RlbSIsIm5iZiI6MTU5MTYxNDI0MiwiZXhwIjoxNTkxNjE2MDQyLCJpYXQiOjE1OTE2MTQyNDIsImlzcyI6InNpa2tlcmhldC1pbnRlcm4uaGVsc2Vub3JnZS5ubyJ9.uPkKdIU0ziFw5GXn8F114GLFOKXUnKm0ETfIm40s_4o"
   )
 
@@ -32,6 +32,7 @@ class ContentAPISimulation extends Simulation{
       .check(regex("access_token\":\"(.*?)\"").saveAs("access_token")))
     */
 
+
     .exec(http("Sykdomsinfo")
       .get("https://eksternapi-hn-mas-01.int-hn.nhn.no/contentapi/v1/Sykdomsinfo")
       .headers(headers_1)
@@ -47,9 +48,10 @@ class ContentAPISimulation extends Simulation{
     case "profile4" => scn.inject(rampConcurrentUsers(5) to(300) during(120))
     case "profile5" => scn.inject(constantConcurrentUsers(10) during (120), rampConcurrentUsers(10) to (100) during (120))
     case "profile6" => scn.inject(incrementUsersPerSec(5).times(5).eachLevelLasting(10).separatedByRampsLasting(10).startingFrom(10))
-
   }
-
   setUp(selectedProfile)
+
+
+  //setUp(scn.inject(constantUsersPerSec(1) during(350))).protocols(httpProtocol)
   }
 
