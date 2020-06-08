2dd0f3d0b53b105c30581dc314929f4766f90aa5
fuglerud <majen78@gmail.com>
2020-06-08 08:48:19 +0200

    lagt til en 0

--- src/test/scala/magnus/ContentAPISimulation.scala
+++ src/test/scala/magnus/ContentAPISimulation.scala
@@ -36,6 +36,7 @@ class ContentAPISimulation extends Simulation{
       .queryParam("Maalgruppe","${maalgruppe}")
       .check(status.is(expected = 200)))
 
+  /*
   val selectedProfile = System.getProperty("selectedProfile") match {
     case "profile1" => scn.inject(atOnceUsers(1))
     case "profile2" => scn.inject(rampUsersPerSec(1) to 5 during (30),constantUsersPerSec(5) during(600))
@@ -44,7 +45,9 @@ class ContentAPISimulation extends Simulation{
     case "profile5" => scn.inject(constantConcurrentUsers(10) during (120), rampConcurrentUsers(10) to (100) during (120))
     case "profile6" => scn.inject(incrementUsersPerSec(5).times(5).eachLevelLasting(10).separatedByRampsLasting(10).startingFrom(10))
   }
+  */
 
-  //setUp(scn.inject(atOnceUsers(1))).protocols(httpProtocol)
+
+  setUp(scn.inject(atOnceUsers(1))).protocols(httpProtocol)
 
 }
