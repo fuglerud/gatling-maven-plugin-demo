121d528c5f52723cb3b64f7e35220a7e0f16b05b
fuglerud <majen78@gmail.com>
2020-09-01 10:37:47 +0200

    fjernet logging

--- src/test/scala/magnus/HelsenorgeFull.scala
+++ src/test/scala/magnus/HelsenorgeFull.scala
@@ -79,7 +79,7 @@ class HelsenorgeFull extends Simulation {
 
   val selectedProfile = System.getProperty("selectedProfile") match {
     case "profile1" => helsenorgeSiteMap.inject(atOnceUsers(1))
-    case "profile2" => helsenorgeSiteMap.inject(rampUsersPerSec(1) to 100 during (15 minutes),constantUsersPerSec(100) during(2 hours))
+    case "profile2" => helsenorgeSiteMap.inject(rampUsersPerSec(1) to 150 during (15 minutes),constantUsersPerSec(150) during(2 hours))
     case "profile3" => helsenorgeSiteMap.inject(constantUsersPerSec(500) during(60))
     case "profile4" => helsenorgeSiteMap.inject(rampConcurrentUsers(5) to(200) during(120))
     case "profile5" => helsenorgeSiteMap.inject(constantConcurrentUsers(10) during (120), rampConcurrentUsers(10) to (100) during (120))
