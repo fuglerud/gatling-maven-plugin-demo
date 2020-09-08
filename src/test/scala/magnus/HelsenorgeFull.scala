621af12ef84bb6052616aa722a72994677e02c8a
fuglerud <majen78@gmail.com>
2020-09-08 12:02:31 +0200

    ny test med morten

--- src/test/scala/magnus/HelsenorgeFull.scala
+++ src/test/scala/magnus/HelsenorgeFull.scala
@@ -143,6 +143,6 @@ class HelsenorgeFull extends Simulation {
   }*/
 
   //setUp(helsenorgeSiteMap.inject(atOnceUsers(1))).protocols(httpProtocol)
-  setUp(helsenorgeSiteMap.inject(rampUsersPerSec(1) to 25 during (30 minutes),constantUsersPerSec(25) during(1 minutes)).protocols(httpProtocol))
+  setUp(helsenorgeSiteMap.inject(rampUsersPerSec(1) to 30 during (30 minutes),constantUsersPerSec(30) during(1 minutes)).protocols(httpProtocol))
   //setUp(helsenorgeSiteMap.inject(constantUsersPerSec(1) during(1481)))
 }
