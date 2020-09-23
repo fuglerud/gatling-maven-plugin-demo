2d8e40bdef5ab0f61e9f61c03577d9c720648071
fuglerud <majen78@gmail.com>
2020-09-23 22:11:37 +0200

    nye testdata sfm basis api

--- src/test/scala/magnus/SFMBasisAPISimulation.scala
+++ src/test/scala/magnus/SFMBasisAPISimulation.scala
@@ -1,15 +1,11 @@
 package magnus
 
-import scala.concurrent.duration._
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
-import io.gatling.jdbc.Predef._
-import org.json4s.jackson.Json
 
 class SFMBasisAPISimulation extends Simulation {
 
   val httpProtocol = http
-    //.baseUrl("https://base-fhir.staging.sfm.cloud")
     .baseUrl("https://base-fhir.qa.forskrivning.no")
     .inferHtmlResources()
     .acceptHeader("application/json")
@@ -26,24 +22,7 @@ class SFMBasisAPISimulation extends Simulation {
     .exec(flushHttpCache)
 
     .feed(csv("magnus/SFMBasisAPI_QA.csv").circular)
-    //.feed(csv("magnus/SFMBasisAPI.csv").circular)
     .feed(csv("magnus/TestdataSFMBasis.csv").circular)
-    //.feed(csv("magnus/TestdataSFMBasis_Staging.csv").circular)
-/*
-    .exec(http("practitioner")
-      .get("/Practitioner/${rekvirent}")
-      .check(status.is(200))
-      .check(jsonPath("$..id").is("${rekvirent}")))
-
-
-    .exec(http("organization")
-      .get("/Organization?name=${organizationname}")
-      .check(status.is(200))
-      .check(jsonPath("$..resource.id").is("${organizationid}")))
-      */
-
-
-
 
     .exec(http("getMedication")
       .post("/Patient/$getMedication")
@@ -56,23 +35,20 @@ class SFMBasisAPISimulation extends Simulation {
       .check(jsonPath("$..[?(@.use==\"official\")].value").saveAs("official")))
 
 
-    .exec(session=>{
-      println("official:")
-      println(session("official").as[String])
-      session})
+    /*.exec(session=>{
+      println("Token: ")
+      println(session("token").as[String])
+      session})*/
 
-
-/*
-    .exec(http("sendMedication")
+    /*.exec(http("sendMedication")
       .post("/Patient/$sendMedication")
       .body(ElFileBody("magnus/SendMedication_request.json"))
-      .check(status.is(200)))
-*/
+      .check(status.is(200)))*/
 
 
-  //setUp(scn.inject(atOnceUsers(1))).protocols(httpProtocol)
+  setUp(scn.inject(atOnceUsers(1))).protocols(httpProtocol)
   //setUp(scn.inject(rampUsersPerSec(1) to 5 during (30),constantUsersPerSec(5) during(60))).protocols(httpProtocol)
-  setUp(scn.inject(constantUsersPerSec(2) during(30))).protocols(httpProtocol)
+  //setUp(scn.inject(constantUsersPerSec(1) during(50))).protocols(httpProtocol)
   //setUp(scn.inject(rampConcurrentUsers(5) to(200) during(120))).protocols(httpProtocol)
   //setUp(scn.inject(constantConcurrentUsers(10) during (120), rampConcurrentUsers(10) to (100) during (120))).protocols(httpProtocol)
   //setUp(scn.inject(incrementUsersPerSec(5).times(5).eachLevelLasting(10).separatedByRampsLasting(10).startingFrom(10))).protocols(httpProtocol)
