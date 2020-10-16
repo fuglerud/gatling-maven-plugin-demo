b0e208721fad2eea5b2e95dd3ffe63e70c2fe565
fuglerud <majen78@gmail.com>
2020-10-16 09:48:45 +0200

    400 pages per second

--- src/test/scala/magnus/SFMBasisAPISimulation.scala
+++ src/test/scala/magnus/SFMBasisAPISimulation.scala
@@ -24,6 +24,17 @@ class SFMBasisAPISimulation extends Simulation {
     .feed(csv("magnus/SFMBasisAPI_QA.csv").circular)
     .feed(csv("magnus/TestdataSFMBasis.csv").circular)
 
+
+/*  .exec(http("practitioner")
+      .get("/Practitioner/${rekvirent}")
+      .check(status.is(200))
+      .check(jsonPath("$..id").is("${rekvirent}")))
+
+    .exec(http("organization")
+      .get("/Organization?name=${organizationname}")
+      .check(status.is(200))
+      .check(jsonPath("$..resource.id").is("${organizationid}")))*/
+
     .exec(http("getMedication")
       .post("/Patient/$getMedication")
       .body(ElFileBody("magnus/0000_request.json"))
@@ -31,25 +42,39 @@ class SFMBasisAPISimulation extends Simulation {
       .check(jsonPath("$..name").is("medication"))
       .check(jsonPath("$..[?(@.name==\"RFM912Feilkode\")].valueCodeableConcept.text").is("OK"))
       .check(jsonPath("$..[?(@.name==\"RFM96Feilkode\")].valueCodeableConcept.text").is("OK"))
-      .check(jsonPath("$..[?(@.name==\"KJFeilkode\")].valueCodeableConcept.text").is("OK"))
+     // .check(jsonPath("$..[?(@.name==\"KJFeilkode\")].valueCodeableConcept.text").is("OK"))
       .check(jsonPath("$..[?(@.use==\"official\")].value").saveAs("official")))
 
-
-    /*.exec(session=>{
-      println("Token: ")
-      println(session("token").as[String])
+   /* .exec(session=>{
+      println("official:")
+      println(session("official").as[String])
       session})*/
 
-    /*.exec(http("sendMedication")
+   /* .exec(http("sendMedication")
       .post("/Patient/$sendMedication")
       .body(ElFileBody("magnus/SendMedication_request.json"))
       .check(status.is(200)))*/
 
 
-  setUp(scn.inject(atOnceUsers(1))).protocols(httpProtocol)
+  /* val selectedProfile = System.getProperty("selectedProfile") match {
+   case "profile1" => scn.inject(atOnceUsers(1))
+   case "profile2" => scn.inject(rampUsersPerSec(1) to 150 during (15 minutes),constantUsersPerSec(150) during(48 hours))
+   case "profile3" => scn.inject(constantUsersPerSec(4) during(30))
+   case "profile4" => scn.inject(rampConcurrentUsers(5) to(200) during(120))
+   case "profile5" => scn.inject(constantConcurrentUsers(10) during (120), rampConcurrentUsers(10) to (100) during (120))
+   case "profile6" => scn.inject(incrementUsersPerSec(5).times(5).eachLevelLasting(10).separatedByRampsLasting(10).startingFrom(10))
+   case "profile7" => scn.inject(rampUsersPerSec(1) to 15 during (3 minutes),constantUsersPerSec(15) during(5 minutes))
+   case "profile8" => scn.inject(constantUsersPerSec(1) during(1481))
+ }*/
+
+
+
+
+  //setUp(scn.inject(atOnceUsers(1))).protocols(httpProtocol)
   //setUp(scn.inject(rampUsersPerSec(1) to 5 during (30),constantUsersPerSec(5) during(60))).protocols(httpProtocol)
-  //setUp(scn.inject(constantUsersPerSec(1) during(50))).protocols(httpProtocol)
+  setUp(scn.inject(constantUsersPerSec(4) during(100))).protocols(httpProtocol)
   //setUp(scn.inject(rampConcurrentUsers(5) to(200) during(120))).protocols(httpProtocol)
   //setUp(scn.inject(constantConcurrentUsers(10) during (120), rampConcurrentUsers(10) to (100) during (120))).protocols(httpProtocol)
   //setUp(scn.inject(incrementUsersPerSec(5).times(5).eachLevelLasting(10).separatedByRampsLasting(10).startingFrom(10))).protocols(httpProtocol)
+
 }
