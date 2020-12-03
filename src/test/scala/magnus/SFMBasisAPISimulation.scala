d056ddebed731e775802cd649dc5dbbbf6cb164c
fuglerud <majen78@gmail.com>
2020-12-03 12:27:19 +0100

    2 sykehus sfm

--- src/test/scala/magnus/SFMBasisAPISimulation.scala
+++ src/test/scala/magnus/SFMBasisAPISimulation.scala
@@ -5,6 +5,8 @@ import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 import io.gatling.jdbc.Predef._
 import org.json4s.jackson.Json
+import java.io.PrintWriter
+import io.gatling.commons.stats.{KO, OK}
 
 class SFMBasisAPISimulation extends Simulation {
 
@@ -20,13 +22,27 @@ class SFMBasisAPISimulation extends Simulation {
     .userAgentHeader("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:75.0) Gecko/20100101 Firefox/75.0")
     .disableCaching
 
+  val writer1: PrintWriter = {
+    val fos = new java.io.FileOutputStream("official.txt")
+    new java.io.PrintWriter(fos, true)}
+
+  val writer2: PrintWriter = {
+    val fos = new java.io.FileOutputStream("responsebodies.txt")
+    new java.io.PrintWriter(fos, true)}
+
+  val writer3: PrintWriter = {
+    val fos = new java.io.FileOutputStream("feiledeFnrOgTokens.txt")
+    new java.io.PrintWriter(fos, true)}
+
   val scn = scenario("SFMBasisAPISimulation")
 
     .exec(flushCookieJar)
     .exec(flushHttpCache)
 
-    .feed(csv("magnus/SFMBasisAPI_QA.csv").circular)
+    .feed(csv("magnus/Tokens.csv").circular)
+    //.feed(csv("magnus/SFMBasisAPI_QA.csv").circular)
     .feed(csv("magnus/TestdataSFMBasis.csv").circular)
+    .feed(csv("magnus/responsebodiesTransformed.csv").circular)
 
 
 /*  .exec(http("practitioner")
@@ -39,6 +55,8 @@ class SFMBasisAPISimulation extends Simulation {
       .check(status.is(200))
       .check(jsonPath("$..resource.id").is("${organizationid}")))*/
 
+
+
     .exec(http("getMedication")
       .post("/Patient/$getMedication")
       .body(ElFileBody("magnus/0000_request.json"))
@@ -46,13 +64,48 @@ class SFMBasisAPISimulation extends Simulation {
       .check(jsonPath("$..name").is("medication"))
       .check(jsonPath("$..[?(@.name==\"RFM912Feilkode\")].valueCodeableConcept.text").is("OK"))
       .check(jsonPath("$..[?(@.name==\"RFM96Feilkode\")].valueCodeableConcept.text").is("OK"))
-      .check(jsonPath("$..[?(@.name==\"KJFeilkode\")].valueCodeableConcept.text").is("OK"))
-      .check(jsonPath("$..[?(@.use==\"official\")].value").saveAs("official")))
+      //.check(jsonPath("$..[?(@.name==\"KJFeilkode\")].valueCodeableConcept.text").is("OK"))
+      .check(jsonPath("$..[?(@.use==\"official\")].value").saveAs("official"))
+      .check(jsonPath("$..*").saveAs( "RESPONSE_DATA") )
+    )
 
-   /* .exec(session=>{
+    .exec((session: io.gatling.core.session.Session) => {
+      if (session.status == KO) {
+        writer3.println(session.attributes("identifier"))
+        writer3.println(session.attributes("token"))
+      }
+      session
+    })
+
+/*
+    .exec(session=>{
       println("official:")
       println(session("official").as[String])
-      session})*/
+      session})
+
+    .exec(session=>{
+      println("RESPONSE_DATA:")
+      println(session("RESPONSE_DATA").as[String])
+      session})
+
+    .exec((session: io.gatling.core.session.Session) => {
+      if (session.status == OK) {
+        writer1.println(session.attributes("official"))
+      }
+      session
+    })
+
+    .exec((session: io.gatling.core.session.Session) => {
+      if (session.status == OK) {
+        writer2.println(session.attributes("RESPONSE_DATA"))
+      }
+      session
+    })
+
+*/
+
+
+
 
    /* .exec(http("sendMedication")
       .post("/Patient/$sendMedication")
@@ -60,23 +113,11 @@ class SFMBasisAPISimulation extends Simulation {
       .check(status.is(200)))*/
 
 
-  /* val selectedProfile = System.getProperty("selectedProfile") match {
-   case "profile1" => scn.inject(atOnceUsers(1))
-   case "profile2" => scn.inject(rampUsersPerSec(1) to 150 during (15 minutes),constantUsersPerSec(150) during(48 hours))
-   case "profile3" => scn.inject(constantUsersPerSec(4) during(30))
-   case "profile4" => scn.inject(rampConcurrentUsers(5) to(200) during(120))
-   case "profile5" => scn.inject(constantConcurrentUsers(10) during (120), rampConcurrentUsers(10) to (100) during (120))
-   case "profile6" => scn.inject(incrementUsersPerSec(5).times(5).eachLevelLasting(10).separatedByRampsLasting(10).startingFrom(10))
-   case "profile7" => scn.inject(rampUsersPerSec(1) to 15 during (3 minutes),constantUsersPerSec(15) during(5 minutes))
-   case "profile8" => scn.inject(constantUsersPerSec(1) during(1481))
- }*/
 
 
-
-
-  //setUp(scn.inject(atOnceUsers(2))).protocols(httpProtocol)
+  setUp(scn.inject(atOnceUsers(1))).protocols(httpProtocol)
   //setUp(scn.inject(rampUsersPerSec(1) to 5 during (30),constantUsersPerSec(5) during(60))).protocols(httpProtocol)
-  setUp(scn.inject(constantUsersPerSec(2) during(60))).protocols(httpProtocol)
+  //setUp(scn.inject(constantUsersPerSec(1) during(5))).protocols(httpProtocol)
   //setUp(scn.inject(rampConcurrentUsers(5) to(200) during(120))).protocols(httpProtocol)
   //setUp(scn.inject(constantConcurrentUsers(10) during (120), rampConcurrentUsers(10) to (100) during (120))).protocols(httpProtocol)
   //setUp(scn.inject(incrementUsersPerSec(5).times(5).eachLevelLasting(10).separatedByRampsLasting(10).startingFrom(10))).protocols(httpProtocol)
---
+++ src/test/scala/magnus/SmitteStopp_ALL.scala
@@ -0,0 +1,72 @@
+package magnus
+
+//import config.Environment.smittestopp_base_url
+import io.gatling.core.Predef._
+import io.gatling.core.feeder.BatchableFeederBuilder
+import io.gatling.core.structure.{PopulationBuilder, ScenarioBuilder}
+import io.gatling.http.Predef._
+import io.gatling.http.request.builder.HttpRequestBuilder
+
+
+class SmitteStopp_ALL extends Simulation {
+
+
+  val httpProtocol = http
+    .baseUrl("https://qa-be-op.ss2np.fhi.no")
+    .inferHtmlResources(BlackList(""".*\.js""", """.*\.css""", """.*\.gif""", """.*\.jpeg""", """.*\.jpg""", """.*\.ico""", """.*\.woff""", """.*\.woff2""", """.*\.(t|o)tf""", """.*\.png""", """.*detectportal\.firefox\.com.*"""), WhiteList())
+    .acceptEncodingHeader("gzip, deflate")
+    .acceptLanguageHeader("en-US,en;q=0.9")
+    .userAgentHeader("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36")
+
+  val header_get = Map(
+    "Content-Type" -> "application/json",
+    "authorization_mobile" -> "24jRFidazK",
+    "User-Agent" -> "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:48.0) Gecko/20100101 Firefox/48.0",
+    "Accept" -> "application/zip",
+    "Accept-Language" -> "fr,en-US;q=0.7,en;q=",
+    "Accept-Encoding" -> "gzip, deflate"
+  )
+
+  val header_post = Map(
+
+    "Accept" -> "application/json; text/plain; application/zip",
+    "Authorization_Mobile" -> "24jRFidazK",
+    "Manufacturer" -> "Apple",
+    "OSVersion" -> "14.2",
+    "OS" -> "IOS",
+    "Host" -> "test-be.ss2np.fhi.no",
+    // "Host" -> "qa-be-op.ss2np.fhi.no",
+    "Authorization" -> "Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IjU1MkUwMjJDRTU4M0I4ODRDNTUyQzZDNUYyNEZFQTFBODc0Rjk0MkMiLCJ0eXAiOiJhdCtqd3QiLCJ4NXQiOiJWUzRDTE9XRHVJVEZVc2JGOGtfcUdvZFBsQ3cifQ.eyJuYmYiOjE2MDYzMTUxMDYsImV4cCI6MTYwNjMxODcwNiwiaXNzIjoiaHR0cHM6Ly9kZXYtc21pdHRlc3RvcHAtdmVyaWZpY2F0aW9uLmF6dXJld2Vic2l0ZXMubmV0IiwiYXVkIjoiaHR0cHM6Ly9kZXYtc21pdHRlc3RvcHAtdmVyaWZpY2F0aW9uLmF6dXJld2Vic2l0ZXMubmV0L3Jlc291cmNlcyIsImNsaWVudF9pZCI6InNtaXR0ZXN0b3BwIiwic3ViIjoiM2IyM2IzZjAtNWE5Zi00NDdiLWIwN2QtM2IwMmZiN2U4ZjRhIiwiYXV0aF90aW1lIjoxNjA2MzE1MTA1LCJpZHAiOiJpZHBvcnRlbiIsImNvdmlkMTlfc3RhdHVzIjoicG9zaXRpdiIsImNvdmlkMTlfYmxva2VyZXQiOiJmYWxzZSIsImNvdmlkMTlfc21pdHRlX3N0YXJ0IjoiMjAyMC0xMS0xMSIsImp0aSI6IkExRjMzNkQ2RkNDMzJFRkJERjAyMDE2MUE2MDA3MkU4Iiwic2lkIjoiQjM0MTAwODM3MjY3NDdDMkVBNjRCNTM3OTgxOTBGRTkiLCJpYXQiOjE2MDYzMTUxMDYsInNjb3BlIjpbIm9wZW5pZCIsInNtaXR0ZXN0b3AiXSwiYW1yIjpbImV4dGVybmFsIl19.dqIs9Bec70AryerFmkLDUFEBVQ3Qyy58U94fJZUyKwMdsHhFMuUrF7wOGX0DSPKgk1yYJxqGSOlqfhJiUzgqSUuWbhtA6NUAOUHemP1bsYT4DuboH5cPJBDPRZU_BwuVK7Zt2jsZE1lhU-xe8_EfaMkiulWEiLLf06O-c-UShfanzQ_nEnN6B9bRGO7nkl_iF3A4i33QY8WBSsRVcdvcoLHqnqg7fWiZlMzshGljZ7-kZi7Y0bC5-2ys6a97KQusw0gSUfe1p4aJJ1yGp2MnE3OTm0NJXizFWNVdUAyR-Fh3EpYpP7hkgcS2ZUX5q4f19lNNFu7qn7kIQwW1e9p6rg"
+  )
+
+  val getConfiguration: HttpRequestBuilder = http("GetConfiguration").get("https://qa-be-op.ss2np.fhi.no/api/v3/diagnostickeys/exposureconfiguration")
+    .headers(header_get)
+    .check(status is 200)
+
+
+  val getKeys: HttpRequestBuilder = http("GetKeys_").get("https://qa-be-op.ss2np.fhi.no/api/v3/diagnostickeys/2020-12-03_1_no.zip")
+    .headers(header_get)
+    .check(status is 200)
+
+
+  val postLogs: HttpRequestBuilder = http("PostLogs").post("https://qa-be-op.ss2np.fhi.no/api/v3/logging/logMessages")
+    .headers(header_post)
+    .check(status is 200)
+
+
+  //val feeder: BatchableFeederBuilder[String]#F = csv("data/smittestopp/keys.csv").circular
+
+  val SCN_SmitteStopp: ScenarioBuilder = scenario("SmitteStopp")
+    //.feed(feeder)
+
+    .exec(getConfiguration)
+    //  .exec(diagnostickeys.body(ElFileBody("bodies/smittestopp/diagnostickeys.json")))
+    .exec(getKeys)
+    .exec(postLogs.body(ElFileBody("magnus/logMessages.json")))
+
+
+
+  setUp(SCN_SmitteStopp.inject(atOnceUsers(1))).protocols(httpProtocol)
+
+
+}
