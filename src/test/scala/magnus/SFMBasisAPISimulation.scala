167f0052ef93fe619ac79243c8a1f3ed54acc2b5
Magnus Jensen Fuglerud <magnus.jensen.fuglerud@nhn.no>
2021-06-29 12:41:07 +0200

    tung peak last

--- src/test/scala/magnus/SFMBasisAPISimulation.scala
+++ src/test/scala/magnus/SFMBasisAPISimulation.scala
@@ -220,7 +220,7 @@ class SFMBasisAPISimulation extends Simulation {
 
     .feed(csv("magnus/TokensLeger.csv").circular)
 
-    .repeat(1800)
+    .repeat(7000)
     {
 
     feed(csv("magnus/SFM_BASIS_40000.csv").circular)
